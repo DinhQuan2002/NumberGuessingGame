bbed2c8c058c1b97fefeec1506789224d1175a60
DinhQuan2002 <quan2483@gmail.com>
2022-04-07 13:08:32 +0700

    first commit

---
+++ NumberGruessingGame/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace NumberGruessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool PlayAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            string response;
+
+            while (PlayAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                number = random.Next(min, max + 1);
+
+                while(guess != number)
+                {
+                    Console.WriteLine("Guess a number between "+min+ "->" +max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+                    if(guess > number)
+                    {
+                        Console.WriteLine(guess + " It's to high!");
+                    }else if(guess < number)
+                    {
+                        Console.WriteLine(guess+" It's to low");
+                    }
+
+                    guesses++;
+                }
+                Console.WriteLine("Number: "+number);
+                Console.WriteLine("YOU WIN!");
+                Console.WriteLine("Guesses: "+guesses);
+
+                Console.WriteLine("Would you like to play again? (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if(response == "Y")
+                {
+                    PlayAgain = true;
+                }
+                else
+                {
+                    PlayAgain = false;
+                }
+
+
+            }
+            Console.WriteLine("Thanks for playing!... I guess");
+
+            Console.ReadKey();
+        }
+    }
+}
